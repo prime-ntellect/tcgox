057b28d8cdf00718b07c50177d926cee9aed7e49
Harrison Beckerich <harrison@beckerich.org>
2021-01-04 23:50:23 -0500

    updates

--- next.config.js
+++ next.config.js
@@ -15,28 +15,26 @@ const nextConfig = {
 	workerLoaderOptions: {},
 };
 
-module.exports = withPwa(
-	withWorkers({
-		...nextConfig,
-		webpack(config, { dev }) {
-			if (dev) {
-				config.devtool = 'cheap-module-source-map';
-			}
+module.exports = withWorkers({
+	...nextConfig,
+	webpack(config, { dev }) {
+		if (dev) {
+			config.devtool = 'cheap-module-source-map';
+		}
 
-			config.resolve.alias = {
-				...config.resolve.alias,
-				'app-components': path.resolve(__dirname, 'components'),
-				'app-configs': path.resolve(__dirname, './configs'),
-				'app-consts': path.resolve(__dirname, './consts'),
-				'app-icons': path.resolve(__dirname, './icons'),
-				'app-layouts': path.resolve(__dirname, './layouts'),
-				'app-modules': path.resolve(__dirname, './modules'),
-				'app-mutations': path.resolve(__dirname, './mutations'),
-				'app-styles': path.resolve(__dirname, './styles'),
-				'app-utils': path.resolve(__dirname, './utils'),
-			};
+		config.resolve.alias = {
+			...config.resolve.alias,
+			'app-components': path.resolve(__dirname, 'components'),
+			'app-configs': path.resolve(__dirname, './configs'),
+			'app-consts': path.resolve(__dirname, './consts'),
+			'app-icons': path.resolve(__dirname, './icons'),
+			'app-layouts': path.resolve(__dirname, './layouts'),
+			'app-modules': path.resolve(__dirname, './modules'),
+			'app-mutations': path.resolve(__dirname, './mutations'),
+			'app-styles': path.resolve(__dirname, './styles'),
+			'app-utils': path.resolve(__dirname, './utils'),
+		};
 
-			return config;
-		},
-	})
-);
+		return config;
+	},
+});
